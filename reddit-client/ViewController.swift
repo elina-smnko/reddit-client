bf58279359443e1028eb04eab4037fbac2a91d72
Elina Semenko <lavrextra@gmail.com>
2020-11-03 11:47:00 +0200

    Initial Commit

---
+++ reddit-client/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  reddit-client
+//
+//  Created by Элина on 03.11.2020.
+//  Copyright © 2020 Элина. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
